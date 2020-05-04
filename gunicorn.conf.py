a27683258f9b98436d325d2c338f5f7db69ff7f4
Jack Mann <jackarthurm@gmail.com>
2020-05-04 15:25:19 +0100

    Update and rename gunicorn.conf to gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,6 @@
 import multiprocessing
 
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = 3  # multiprocessing.cpu_count() * 2 + 1
 secure_scheme_headers = {'X-FORWARDED-PROTO': 'https'}
 keepalive = 75
 timeout = 90
