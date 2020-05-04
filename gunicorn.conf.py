e80a3bce05b0a8ffa159ba6b6e4dab81378e9471
Jack Mann <jackarthurm@gmail.com>
2020-05-04 18:19:24 +0100

    Delete gunicorn.conf.py

--- gunicorn.conf.py
+++
@@ -1,7 +0,0 @@
-import multiprocessing
-
-workers = 3  # multiprocessing.cpu_count() * 2 + 1
-secure_scheme_headers = {'X-FORWARDED-PROTO': 'https'}
-keepalive = 75
-timeout = 90
-worker_class = 'gevent'
